e65bc2f8f4692d821ea3c656180db0b05b24ee89
melissa-studi <melissa_bazir@yahoo.fr>
2023-11-05 07:55:26 +0100

    CRUD User creation

---
+++ src/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Company;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('lastname')
+            ->add('firstname')
+            ->add('telephone')
+            ->add('created_at')
+            ->add('company', EntityType::class, [
+                'class'=> Company::class,
+                'choice_label'=> 'name'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
