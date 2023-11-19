352333af28f20815ea2de4bbf0ebf1176cb6dc39
melissa-studi <melissa_bazir@yahoo.fr>
2023-11-19 15:57:41 +0100

    Hiding approved field for visitors in new review form

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -7,12 +7,13 @@ use App\Entity\Review;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 
 class ReviewType extends AbstractType
